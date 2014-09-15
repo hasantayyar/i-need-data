44fae220a23f21d30493f3063a2ff1d64a0cceb5
hasantayyar <tayyar.besik@gmail.com>
2014-09-15 14:19:09 +0300

    kitaplar step1 - yazar sayfaları

---
+++ Kitapyurdu/fetchAuthorPages.php
@@ -0,0 +1,35 @@
+<?php
+require './lib/ganon.php';
+$base = "http://www.kitapyurdu.com";
+
+fetch($base);
+
+function fetch($base){
+	$alphas = range('a', 'z');
+	$alphas = array_merge( array('%F6','%FC','%FE','%FD') ,$alphas);
+	foreach($alphas as $alpha){
+	$link = $base.'/yazar/default.asp?session=87666E24-522B-4055-8B2A-83B6BAD7BBF2&LogID=&arama=ad&option='.$alpha;
+	echo "Processing $link\n\n";
+	try 
+	{
+	    $m = new Mongo(); // connect
+	    $db = $m->selectDB("books");
+	}
+	catch ( MongoConnectionException $e ) 
+	{
+	    echo 'db error';
+	    exit();
+	}
+	$html = file_get_dom($link);
+	if(!empty($html)){
+		// get next page
+		$authors = $html('tr td font a');
+		foreach($authors as $author){
+		  if(stristr($author->href,'yazar')){
+		 		$url = $base.$author->href;
+				echo $url."\n"; 		
+			} 
+		}
+	}
+	}
+}
