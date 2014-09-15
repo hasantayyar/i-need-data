3d24dee55e6f6fe830f2f4dd1519a738fef2ab0f
hasantayyar <tayyar.besik@gmail.com>
2014-09-16 00:09:27 +0300

    segfault fix while getting big lists

--- kitapyurdu.com/fetchAuthorPages.php
+++ kitapyurdu.com/fetchAuthorPages.php
@@ -1,4 +1,10 @@
 <?php
+ini_set("max_execution_time", "0");
+ini_set("memory_limit", "2G");
+ini_set('implicit_flush', 1);
+set_time_limit(0);
+
+
 require './../lib/ganon.php';
 
 $base = "http://www.kitapyurdu.com";
@@ -24,19 +30,22 @@ function fetch($base)
 	foreach($alphas as $alpha) {
 		$link = $base . '/yazar/default.asp?session=87666E24-522B-4055-8B2A-83B6BAD7BBF2&LogID=&arama=ad&option=' . $alpha;
 		echo "Processing $link\n\n";
-
 		$html = file_get_dom($link);
 		if (!empty($html)) {
 			$authors = $html('tr td font a');
-			foreach($authors as $author) {
+			$html->clear();
+			unset($html);
+			for($i=0;$i<count($authors);++$i){
+				$author = $authors[$i];
 				if (stristr($author->href, 'yazar')) {
 					$url = $base . $author->href;
 					echo $url . "\n";
 					$db->authors->insert(array('url'=>$url));
 				}
+				unset($author);
 			}
 			unset($authors);
 		}
-		unset($html);
+		sleep(1);
 	}
 }
